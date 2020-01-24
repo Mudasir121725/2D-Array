466087c1afcd974d0bc9b63f953368cb56d7ad3b
Mudasir Zamani <54984236+Mudasir121725@users.noreply.github.com>
2020-01-24 23:23:45 +0530

    Add files via upload

---
+++ 2D Array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,a[3][3],b[3][3],c[3][3];
+    printf("Enter number for first Array :");
+    for(i=0;i<=2;i++)
+    {
+    for(j=0;j<=2;j++)
+    {
+         scanf("%d",&a[i][j]);
+    }}
+            printf("Enter number for second Array :\n");
+    for (i=0;i<=2;i++)
+    {
+    for (j=0;j<=2;j++)
+        {
+             scanf("%d",&b[i][j]);
+        }}
+    for (i=0;i<=2;i++)
+    {
+    for (j=0;j<=2;j++)
+    {
+        c[i][j]=a[i][j]+b[i][j];
+        printf("%d ",c[i][j]);
+    }
+    printf("\n");
+}
+}
